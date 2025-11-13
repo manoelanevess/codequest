e2eb3656bedb6d377ab1edcbfe62328945184916
manoela <manoela.nevesr@gmail.com>
2025-11-13 14:34:01 -0300

    unit: ajust erro from router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:24}}>
+      <h1>Olá CodeQuest </h1>
+      <p>Erick se tu ta vendo isso agr ta funcionando </p>
+    </div>
+  );
+}
